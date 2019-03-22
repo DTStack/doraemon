b2cf9d7967d63b0bd27628b0a7a71e7b80271d72
长卿 <changqing@dtstack.com>
2019-03-22 20:58:47 +0800

    再次改

--- app/web/pages/proxyServer/index.js
+++ app/web/pages/proxyServer/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Row,Col,Button,Table,message as Message, Divider, Modal,Badge,Popconfirm} from 'antd';
 import {API} from '@/api';
-import ProxyServerModal from './components/proxy';
+import ProxyServerModal from './components/prox';
 import ProxyRuleModal from './components/proxyRuleModal';
 import './style.scss';
 
