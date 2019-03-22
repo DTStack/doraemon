6b9c4e7189c8e07b281c22e4b303f5c6880ff18c
长卿 <changqing@dtstack.com>
2019-03-22 21:03:28 +0800

    标题链接首页

--- app/web/layouts/basicLayout/index.js
+++ app/web/layouts/basicLayout/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Layout} from 'antd';
+import {Link} from 'react-router-dom';
 import classnames from 'classnames';
 import './style.scss';
-const {Header, Content,Footer} = Layout;
+const {Header, Content} = Layout;
 export default class BasicLayout extends React.PureComponent{
   render(){
     const {className} = this.props;
     return (
       <Layout className="layout-basic">
         <Header style={{ padding: '0 10px' }}>
-          <span className="system-title">哆啦A梦</span>
+          <Link to='/page/home'>
+            <span className="system-title">哆啦A梦</span>
+          </Link>
         </Header>
         <Content className={classnames("main-content",className)}>
           {this.props.children}
