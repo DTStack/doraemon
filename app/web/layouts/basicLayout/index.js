02422de135bbdac4a0d6f1c7077c1dbc2ce95b97
长卿 <changqing@dtstack.com>
2019-08-16 19:17:38 +0800

    加入本机ip的获取

--- app/web/layouts/basicLayout/index.js
+++ app/web/layouts/basicLayout/index.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import {Layout} from 'antd';
+import {Layout,Row,Col} from 'antd';
 import {Link} from 'react-router-dom';
 import classnames from 'classnames';
+import {getIPs} from '@/utils';
 import './style.scss';
 const {Header, Content} = Layout;
 export default class BasicLayout extends React.PureComponent{
+  state = {
+    clientIp:''
+  }
+  componentDidMount(){
+    getIPs().then((ip)=>{
+      this.setState({
+        clientIp:ip
+      })
+    });
+  }
   render(){
+    const {clientIp} = this.state;
     const {className} = this.props;
     return (
       <Layout className="layout-basic">
         <Header style={{ padding: '0 10px' }}>
-          <Link to='/page/home'>
-            <span className="system-title">哆啦A梦</span>
-          </Link>
+          <Row>
+            <Col span={12}>
+              <Link to='/page/home'>
+                <span className="system-title">哆啦A梦</span>
+              </Link>
+            </Col>
+            <Col span={12} style={{textAlign:'right'}}><span className="clientip">{clientIp}</span></Col>
+          </Row>
         </Header>
         <Content className={classnames("main-content",className)}>
           {this.props.children}
