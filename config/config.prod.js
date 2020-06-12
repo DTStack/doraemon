4fcfca8936540ff8366f1c3ea7df6c3cc5759f59
linhe <linhe@dtstack.com>
2020-06-12 11:49:38 +0800

    Update config.prod.js 修改数据库密码

--- config/config.prod.js
+++ config/config.prod.js
@@ -17,7 +17,7 @@ module.exports = () => {
         host: '127.0.0.1',//172.16.8.163
         port: 3306,
         username:'root',
-        password:'admin'
+        password:'Admin123!@#'
       }
     ]
   };
