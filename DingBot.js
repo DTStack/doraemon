f4e1f62f31ff99115ac7e83790ca52ae175193eb
nankanull <534129087@qq.com>
2021-06-30 23:36:33 +0800

    fix: (?.) can not use under node.js v14.15.0

--- DingBot.js
+++ DingBot.js
@@ -3,7 +3,7 @@ const version = require('./package.json').version;
 const config = require('./env.json');
 const Logger = require('egg-logger').Logger;
 const logger = new Logger('DingdingBot');
-const botWebhookUrl = config?.webhookUrls || []; //钉钉通知群
+const botWebhookUrl = config && config.webhookUrls ? config.webhookUrls : []; //钉钉通知群
 const ApplicationTypeHeader = 'application/json;charset=utf-8';
 // DingdingBot
 class DingdingBot {
@@ -26,7 +26,7 @@ class DingdingBot {
                         'text': `### Doraemon v${version} 发布成功 ![screenshot](https://img.pngio.com/my-impression-doraemon-is-my-favorite-cartoon-l-like-doraemon-doraemon-png-820_443.png) \n>致力于解放更多生产力，让日常工作变的更加高效、轻松、便捷、愉快～`,
                         'btnOrientation': '0',
                         'singleTitle': '阅读全文',
-                        'singleURL': config?.msgSingleUrl || ''
+                        'singleURL': config && config.msgSingleUrl ? config.msgSingleUrl : ''
                     }
                 }
             };
