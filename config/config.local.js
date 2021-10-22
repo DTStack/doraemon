ac96c0aa79083bbdbc9374b72e5c43c268065f76
liuyi <liuyi@dtstack.com>
2021-10-22 11:27:37 +0800

    feat: local database name

--- config/config.local.js
+++ config/config.local.js
@@ -34,12 +34,12 @@ module.exports = () => {
             {
                 delegate: 'model',
                 baseDir: 'model',
-                database: 'doraemon',
+                database: 'doraemon_test',
                 dialect: 'mysql',
                 host: '127.0.0.1',
                 port: 3306,
-                username:'root',
-                password:''
+                username: 'root',
+                password: ''
             }
         ]
     };
