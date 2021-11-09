40ba60295332096e45ff2da79ab6f219b14d0968
liuyi <liuyi@dtstack.com>
2021-11-09 10:23:40 +0800

    feat: get timedTask when app start

--- app/schedule/articleSubscription.js
+++ app/schedule/articleSubscription.js
@@ -2,8 +2,7 @@ module.exports = {
     schedule: {
         interval: '0 0 1 ? * SAT',
         type: 'worker',
-        immediate: true,
-        disable: true // 这个定时任务不会被启动
+        immediate: true
     },
     async task(ctx) {
         ctx.service.articleSubscription.startSubscriptionTimedTask()
