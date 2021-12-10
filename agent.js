296437940d2648bd63e2a87973f0e27ca1ad0454
liuyi <liuyi@dtstack.com>
2021-12-10 10:52:08 +0800

    feat: improve timed task result log

--- agent.js
+++ agent.js
@@ -1,24 +1,29 @@
-const { createTimedTask, changeTimedTask, cancelTimedTask, timedTaskList } = require('./app/utils/timedTask')
+const { createTimedTask, changeTimedTask, cancelTimedTask, timedTaskList, timedTaskResult } = require('./app/utils/timedTask')
 
 // 接收 app 发送来的消息并作出反应
 module.exports = agent => {
-    // 创建定时任务
+    // 创建文章订阅任务
     agent.messenger.on('createTimedTask', ({ id, sendCron }) => {
         createTimedTask(id, sendCron, agent)
     })
 
-    // 改变定时任务
+    // 改变文章订阅任务
     agent.messenger.on('changeTimedTask', ({ id, sendCron }) => {
         changeTimedTask(id, sendCron, agent)
     })
 
-    // 取消定时任务
+    // 取消文章订阅任务
     agent.messenger.on('cancelTimedTask', ({ id }) => {
         cancelTimedTask(id, agent)
     })
 
-    // 定时任务列表
+    // 文章订阅任务列表
     agent.messenger.on('timedTaskList', () => {
         timedTaskList(agent)
+    })
+
+    // 打印文章订阅任务的执行结果
+    agent.messenger.on('timedTaskResult', ({ result }) => {
+        timedTaskResult(result, agent)
     })
 }
