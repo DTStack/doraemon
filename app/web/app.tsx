faeb46ed14fa74c6bbd0f73233a2730a43d27c27
liuyi <>
2023-07-20 22:59:19 +0800

    feat: remove hotjar

--- app/web/app.tsx
+++ app/web/app.tsx
@@ -15,18 +15,7 @@ declare var window: any;
 
 const App = () => {
     const { changeLocalIp } = bindActionCreators(actions, useDispatch());
-    const hotJar = () => {
-        (function (h: any, o: any, t: any, j: any, a?: any, r?: any) {
-            h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
-            h._hjSettings = { hjid: 2133522, hjsv: 6 };
-            a = o.getElementsByTagName('head')[0];
-            r = o.createElement('script'); r.async = 1;
-            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
-    }
     useEffect(() => {
-        hotJar();
         changeLocalIp();
     }, [])
     return (
