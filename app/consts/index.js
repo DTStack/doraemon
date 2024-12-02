a1eb06ddb03688ac56485a93fbe35f6424faa9e9
liuxy0551 <liuxy0551@qq.com>
2024-12-02 11:05:08 +0800

    feat: use CommonJS

--- app/consts/index.js
+++ app/consts/index.js
@@ -1,12 +1,17 @@
 // 订阅网站的一级分类
-export const SITE_NAME = {
+const SITE_NAME = {
     GITHUB: 'Github',
     JUEJIN: '掘金',
     ENGLISH: '英文'
 }
 
 // 订阅网站的二级分类 - 话题
-export const TOPIC_NAME = {
+const TOPIC_NAME = {
     DEV_ARCHITECTURE: 'DEV Architecture',
     REACT_STATUS: 'React Status'
+}
+
+module.exports = {
+    SITE_NAME,
+    TOPIC_NAME
 }
