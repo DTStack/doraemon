a09949ca1f208fefefa9a2aa99b0358c3388ad7e
liuxy0551 <liuxy0551@qq.com>
2024-12-03 15:04:45 +0800

    ci: lint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: [require.resolve('ko-lint-config/.eslintrc')],
+    rules: {
+        'no-param-reassign': 0,
+        'prefer-rest-params': 0,
+        'no-octal-escape': 0,
+        'new-cap': 0,
+        'no-new': 0,
+        'react/no-unescaped-entities': 0,
+        'no-control-regex': 0,
+    },
+};
---
+++ .stylelintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: ['ko-lint-config/.stylelintrc'],
+    rules: {
+        'value-no-vendor-prefix': null,
+    },
+};
