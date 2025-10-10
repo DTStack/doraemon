740d2b3b0f19a41508e96227a23e53c7ed60df50
JackWang032 <2522134117@qq.com>
2025-10-10 16:31:35 +0800

    fix: revert database config

--- config/config.local.js
+++ config/config.local.js
@@ -34,12 +34,12 @@ module.exports = () => {
             {
                 delegate: 'model',
                 baseDir: 'model',
-                database: 'doraemon',
+                database: 'doraemon_test',
                 dialect: 'mysql',
                 host: '127.0.0.1',
                 port: 3306,
                 username: 'root',
-                password: '123456789',
+                password: '123456',
             },
         ],
     };
